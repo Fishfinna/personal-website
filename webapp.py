8fa4ccf56cb9807df952674a9496b488d97f67b9
Shanti <fishfinna12@gmail.com>
2022-06-25 16:13:13 -0700

    added boopers

--- webapp.py
+++ webapp.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify, redirect, render_template
-
+from flask import Flask, jsonify, send_file, redirect, render_template, current_app
+import os
 # flask set up
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/static/resources"
 
 
 @app.route("/")
 def home():
     return render_template("homepage.html")
+
+
+@app.route("/download/<file>")
+def download(file):
+    return send_file(f"{current_app.root_path + app.config['UPLOAD_FOLDER']}/{file}")
 
 
 if __name__ == "__main__":
